c139365846303e81fe93d7cca57efd4c7dc2080e
Alexander Cherednichenko <lexaux@gmail.com>
2013-01-24 09:45:03 +0200

    Updated logging a little

--- src/com/lexaux/scala/akka/actors/SecondActor.scala
+++ src/com/lexaux/scala/akka/actors/SecondActor.scala
@@ -15,11 +15,13 @@ class SecondActor extends Actor {
   val log = Logging(context.system, this)
 
   val creationTime = System.currentTimeMillis()
-  log.info("Created SecondActor " + this)
 
   def receive = {
     case "Ping2" => sender ! new Data(17)
-    case TestMessage(x) => sender ! new Data(x + 1)
+    case TestMessage(x) => {
+      log.warning("INVOKING SecondActor with message " + x)
+      sender ! new Data(x + 1)
+    }
     case _ => log.warning("DONT KNOW HOW TO HANDLE")
   }
 }
