4fc989ec11807a7afc3310beab6a465835305eb0
Alexander Cherednichenko <lexaux@gmail.com>
2013-01-26 20:10:00 +0200

    #1 moving to SBT -- intermediate commit.

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Roadworks REST api"
+
+version := "0.1"
+
+scalaVersion := "2.10.0"
--- src/com/lexaux/scala/akka/servlet/AkkaApplication.scala.scala
+++ src/com/lexaux/scala/akka/servlet/AkkaApplication.scala.scala
@@ -1,13 +1,41 @@
 package com.lexaux.scala.akka.servlet
 
-import javax.servlet.{ServletContextEvent, ServletContextListener}
+import scala.slick.driver.PostgresDriver._
 import akka.actor.ActorSystem
 import grizzled.slf4j.Logging
+import slick.session.Database
+import javax.servlet.{ServletContextEvent, ServletContextListener}
 
-/**
- * Akka application setup here.
- */
+// Use H2Driver to connect to an H2 database
+import scala.slick.driver.H2Driver.simple._
+
+// Use the implicit threadLocalSession
+import Database.threadLocalSession
+
 class AkkaServletContextListener extends ServletContextListener with Logging {
+
+  /**
+   * Akka application setup here.
+   */
+  object Suppliers extends Table[(Int)]("SUPPLIERS") {
+    def id = column[Int]("SUP_ID", O.PrimaryKey)
+
+    def * = id //~ name ~ street ~ city ~ state ~ zip
+  }
+
+  object Customers extends Table[(Int)]("CUSTOMERS") {
+    def id = column[Int]("SUP_ID", O.PrimaryKey)
+
+    def * = id //~ name ~ street ~ city ~ state ~ zip
+  }
+
+  def testDB() {
+    Database.forURL("jdbc://postgresql/localhost/slicktest", "user", "password", driver = "org.postgresql.Driver") withSession {
+      (Suppliers.ddl ++ Customers.ddl).
+    }
+  }
+
+  testDB()
 
   def contextDestroyed(p1: ServletContextEvent) {
     info("Hello! Staring the demolition.")
